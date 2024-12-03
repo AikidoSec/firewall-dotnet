472647c1d46b059d907c34c4f32d8d25afcdbde4
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-03 17:15:59 +0100

    Better scripts for local nuget pakcages

--- pack.ps1
+++ pack.ps1
@@ -1,12 +1,12 @@
 param (
     [string]$buildConfiguration = "Debug"
 )
-
 # Define locations
 $outputPath = "../nupkgs"
 $coreProject = "../Aikido.Zen.Core/Aikido.Zen.Core.csproj"
 $dotNetFrameworkProject = "../Aikido.Zen.DotNetFramework/packages.config"
 $dotNetCoreProject = "../Aikido.Zen.DotNetCore/Aikido.Zen.DotNetCore.csproj"
+$frameworkSampleApps = @("../sample-apps/sql-injection-framework/packages.config")
 
 # pack core without building
 Write-Host "Packing Aikido.Zen.Core..."
@@ -23,13 +23,14 @@ $latestVersion = $latestPackage.Name -replace '^Aikido\.Zen\.Core\.|\.nupkg$'
 Write-Host "Latest Aikido.Zen.Core version: $latestVersion"
 
 # Update the version in the dependent projects
-(Get-Content $dotNetCoreProject) -replace '(?i)(?<=<PackageReference Include="Aikido.Zen.Core" version=")[^"]+(?=")', $latestVersion | Set-Content $dotNetCoreProject
-(Get-Content $dotNetFrameworkProject) -replace '(?i)(?<=<package id="Aikido.Zen.Core" version=")[^"]+(?=")', $latestVersion | Set-Content $dotNetFrameworkProject
+Write-Host "Updating $dotNetCoreProject with Aikido.Zen.Core v$latestVersion"
+dotnet add $dotNetCoreProject package 'Aikido.Zen.Core' --version $latestVersion
+Write-Host "Updating $dotNetFrameworkProject with Aikido.Zen.Core v$latestVersion"
+dotnet add $dotNetFrameworkProject package 'Aikido.Zen.Core' --version $latestVersion
 
 
-# Build and pack the dependent projects
-foreach ($project in $projects) {
-    dotnet restore $project
-    dotnet build $project --configuration $buildConfiguration
-    dotnet pack $project --configuration $buildConfiguration -o $outputPath
+# update the version in the sample apps
+foreach ($app in $frameworkSampleApps) {
+    Write-Host "Updating $app with Aikido.Zen.Core v$latestVersion"
+    dotnet add $app package 'Aikido.Zen.Core' --version $latestVersion
 }
