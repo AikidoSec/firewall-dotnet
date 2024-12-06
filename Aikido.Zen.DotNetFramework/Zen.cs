67a9c5e0b1e683ff0a1e0d4c0f349b831845bd79
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-06 11:23:03 +0100

    Accomodated for the new singleton agent setup

--- Aikido.Zen.DotNetFramework/Zen.cs
+++ Aikido.Zen.DotNetFramework/Zen.cs
@@ -7,24 +7,19 @@ namespace Aikido.Zen.DotNetFramework
 {
     public class Zen
     {
-    private static Agent _agent;
-        public static Agent Agent {
-            get {
-                if (_agent == null)
-                {
-                    var baseUrl = Environment.GetEnvironmentVariable("AIKIDO_URL") ?? "https://guard.aikido.dev";
-                    var uri = new Uri(baseUrl);
-                    var apiClient = new ReportingAPIClient(uri);
-                    var zenApi = new ZenApi(apiClient);
-                    _agent = new Agent(zenApi);
-                }
-				return _agent;
-			}
-        }
-
         public static void Start() {
-            // Send the started event
-            Agent.Start(AikidoConfiguration.Options.AikidoToken);
+        // patch the sinks
+        Patcher.Patch();
+        // setup the agent
+        if (Agent.Instance == null)
+        {
+            var baseUrl = Environment.GetEnvironmentVariable("AIKIDO_URL") ?? "https://guard.aikido.dev";
+            var uri = new Uri(baseUrl);
+            var apiClient = new ReportingAPIClient(uri);
+            var zenApi = new ZenApi(apiClient);
+            Agent.GetInstance(zenApi);
+        }
+        Agent.Instance.Start(AikidoConfiguration.Options.AikidoToken);
         }
     }
 }
