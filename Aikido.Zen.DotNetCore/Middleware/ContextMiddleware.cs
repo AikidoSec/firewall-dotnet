08712ed4afbb6bd80aeea4caa4954afd6c818b9b
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-09 17:02:40 +0100

    Added user input parsing to thr context middleware

--- Aikido.Zen.DotNetCore/Middleware/ContextMiddleware.cs
+++ Aikido.Zen.DotNetCore/Middleware/ContextMiddleware.cs
@@ -1,28 +1,28 @@
 using Aikido.Zen.Core;
-using Aikido.Zen.Core.Api;
+using Aikido.Zen.Core.Helpers;
 using Microsoft.AspNetCore.Http;
 
 namespace Aikido.Zen.DotNetCore.Middleware
 {
-	public class ContextMiddleware : IMiddleware
-	{
-		public ContextMiddleware()
-		{
-		}
+    public class ContextMiddleware : IMiddleware
+    {
+        public ContextMiddleware()
+        {
+        }
 
-		public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
-		{
+        public async Task InvokeAsync(HttpContext httpContext, RequestDelegate next)
+        {
             // this will be used to check for attacks
-			var context = new Context
-			{
-				Url = httpContext.Request.Path.ToString(),
-				Method = httpContext.Request.Method,			
-				Query = httpContext.Request.Query.ToDictionary(q => q.Key, q => q.Value.ToArray()),
-				Headers = httpContext.Request.Headers
-					.ToDictionary(h => h.Key, h => h.Value.ToArray()),
-				RemoteAddress = httpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty,
-				Cookies = httpContext.Request.Cookies.ToDictionary(c => c.Key, c => c.Value)
-			};
+            var context = new Context
+            {
+                Url = httpContext.Request.Path.ToString(),
+                Method = httpContext.Request.Method,
+                Query = httpContext.Request.Query.ToDictionary(q => q.Key, q => q.Value.ToArray()),
+                Headers = httpContext.Request.Headers
+                    .ToDictionary(h => h.Key, h => h.Value.ToArray()),
+                RemoteAddress = httpContext.Connection.RemoteIpAddress?.ToString() ?? string.Empty,
+                Cookies = httpContext.Request.Cookies.ToDictionary(c => c.Key, c => c.Value)
+            };
 
             // no need to use X-FORWARDED-FOR, .NET Core already handles this
             var clientIp = httpContext.Connection.RemoteIpAddress?.ToString();
@@ -30,25 +30,34 @@ namespace Aikido.Zen.DotNetCore.Middleware
             // every x minutes, this information will be sent to the Zen server as a heartbeat event, and the collected info will be cleared
             Agent.Instance.CaptureInboundRequest(context.User, httpContext.Request.Path, context.Method, clientIp);
 
-			if (httpContext.Request.ContentLength > 0)
-			{
-				try
-				{
-                    // we need to leave the body stream unread, so we copy it to a buffer
-                    // and then replace the body with a new memory stream that we can read multiple times
-                    var buffer = new byte[httpContext.Request.ContentLength.Value];
-                    await httpContext.Request.Body.ReadAsync(buffer, 0, buffer.Length);
-                    context.Body = System.Text.Encoding.UTF8.GetString(buffer);
-                    httpContext.Request.Body = new MemoryStream(buffer);
-				}
-				catch (Exception)
-				{
-					httpContext.Request.Body.Position = 0;
-				}
-			}
+            if (httpContext.Request.ContentLength > 0)
+            {
+                try
+                {
+                    var request = httpContext.Request;
+                    // allow the body to be read multiple times
+                    request.EnableBuffering();
+                    var parsedUserInput = await HttpHelper.ReadAndFlattenHttpDataAsync(
+                        queryParams: context.Query.ToDictionary(h => h.Key, h => string.Join(',', h.Value)),
+                        headers: context.Headers.ToDictionary(h => h.Key, h => string.Join(',', h.Value)),
+                        cookies: context.Cookies,
+                        body: request.Body,
+                        contentType: request.ContentType,
+                        contentLength: request.ContentLength ?? 0
+                    );
+                    context.ParsedUserInput = parsedUserInput;
 
-			httpContext.Items["Aikido.Zen.Context"] = context;
+                }
+                catch (Exception e)
+                {
+                    var message = e.Message;
+                    var trace = e.StackTrace;
+                    throw;
+                }
+            }
+
+            httpContext.Items["Aikido.Zen.Context"] = context;
             await next(httpContext);
-		}
-	}
+        }
+    }
 }
