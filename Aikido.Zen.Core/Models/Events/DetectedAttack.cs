26dde266fed5a2674564e1ec5a4e55cb619ad3ab
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-18 16:05:59 +0100

    Fixed detectedAttack event

--- Aikido.Zen.Core/Models/Events/DetectedAttack.cs
+++ Aikido.Zen.Core/Models/Events/DetectedAttack.cs
@@ -1,10 +1,12 @@
+using Aikido.Zen.Core.Helpers;
+
 namespace Aikido.Zen.Core.Models.Events {
     public class DetectedAttack : IEvent
     {
-        public const string Type = "detected_attack";
+        public string Type => "detected_attack";
         public RequestInfo Request { get; set; }
         public Attack Attack { get; set; }
         public AgentInfo Agent { get; set; }
-        public long Time { get; set; }
+        public long Time => DateTimeHelper.UTCNowUnixMilliseconds();
     }
 }
