f6e308fcb03bc87ec54262f3af223434caf1b442
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-18 16:17:19 +0100

    expanded helpers

--- Aikido.Zen.Core/Helpers/EnvironmentHelper.cs
+++ Aikido.Zen.Core/Helpers/EnvironmentHelper.cs
@@ -7,5 +7,9 @@ namespace Aikido.Zen.Core.Helpers
     {
         public static string Token => Environment.GetEnvironmentVariable("AIKIDO_TOKEN");
         public static bool DryMode => Environment.GetEnvironmentVariable("AIKIDO_BLOCKING") != "true";
+        public static string AikidoUrl => Environment.GetEnvironmentVariable("AIKIDO_URL")
+            ?? "https://guard.aikido.dev";
+        public static string AikidoRealtimeUrl => Environment.GetEnvironmentVariable("AIKIDO_REALTIME_URL")
+            ?? "https://runtime.aikido.dev";
     }
 }
