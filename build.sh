c6fb4b61c2bdd70d3a9abc545fe493e8e5296b82
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-23 14:56:22 +0100

    Updated the build.sh and unit test yaml files

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
-set -euox pipefail
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+# Get script directory, equivalent to PowerShell's $PSScriptRoot
+scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$scriptPath"
+
+# Equivalent to PowerShell's $ErrorActionPreference = 'Stop'
+set -e
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
 
 dotnet tool restore
+# Check if the tool restore failed
+if [ $? -ne 0 ]; then exit $?; fi
 
 dotnet cake "$@"
+# Check if the cake build failed
+if [ $? -ne 0 ]; then exit $?; fi
