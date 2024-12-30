bdda57a258d95d93d332c3c6a5892aadc88c2137
Yannis Standaert <yannis.standaert@wearetriple.com>
2024-12-30 14:31:54 +0100

    refactor of drymode logic

--- Aikido.Zen.Core/Helpers/EnvironmentHelper.cs
+++ Aikido.Zen.Core/Helpers/EnvironmentHelper.cs
@@ -6,7 +6,16 @@ namespace Aikido.Zen.Core.Helpers
     public static class EnvironmentHelper
     {
         public static string Token => Environment.GetEnvironmentVariable("AIKIDO_TOKEN");
-        public static bool DryMode => Environment.GetEnvironmentVariable("AIKIDO_BLOCKING") != "true" && Environment.GetEnvironmentVariable("AIKIDO_BLOCKING") != "1";
+        public static bool DryMode => GetDryMode();
+
+        private static bool GetDryMode()
+        {
+            var blocking = Environment.GetEnvironmentVariable("AIKIDO_BLOCKING");
+            if (blocking == "true" || blocking == "1")
+                return false;
+            return true;
+        }
+        
         public static string AikidoUrl => Environment.GetEnvironmentVariable("AIKIDO_URL")
             ?? "https://guard.aikido.dev";
         public static string AikidoRealtimeUrl => Environment.GetEnvironmentVariable("AIKIDO_REALTIME_URL")
