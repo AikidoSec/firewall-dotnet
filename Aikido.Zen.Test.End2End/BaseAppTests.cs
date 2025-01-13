fb2a314bcfc506226424cf5504ab44424b071764
Yannis Standaert <yannis@quickminds.be>
2025-01-13 10:23:44 +0100

    trying network mode bridge for docker containers

--- Aikido.Zen.Test.End2End/BaseAppTests.cs
+++ Aikido.Zen.Test.End2End/BaseAppTests.cs
@@ -46,6 +46,7 @@ public abstract class BaseAppTests
     {
         // Create network first
         Network = new NetworkBuilder()
+            .WithDriver(NetworkDriver.Bridge)
             .WithName(NetworkName)
             .Build();
         Network.CreateAsync().Wait();
