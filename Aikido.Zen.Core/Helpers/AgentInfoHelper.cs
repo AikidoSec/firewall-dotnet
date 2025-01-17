30455b07fff80c1900f4bc3a8bb34596878f6d3d
Yannis Standaert <yannis@quickminds.be>
2025-01-17 10:34:14 +0100

    small perf improvements for better benchmark results

--- Aikido.Zen.Core/Helpers/AgentInfoHelper.cs
+++ Aikido.Zen.Core/Helpers/AgentInfoHelper.cs
@@ -9,34 +9,37 @@ namespace Aikido.Zen.Core.Helpers
 	/// </summary>
 	internal class AgentInfoHelper
 	{
+		// Cache the AgentInfo object to avoid repeated creation
+		private static readonly AgentInfo _cachedAgentInfo = new AgentInfo
+		{
+			Hostname = Environment.MachineName,
+			Os = new Os
+			{
+				Version = Environment.OSVersion.VersionString,
+				Name = Environment.OSVersion.Platform.ToString()
+			},
+			Platform = new Platform
+			{
+				Version = Environment.Version.ToString(),
+				Arch = Environment.Version.Major >= 5 ? "core" : "framework"
+			},
+			IpAddress = IPHelper.Server,
+			Library = "firewall-dotnet",
+			Version = "1.0.0",
+			// Determine if running in a serverless environment
+			Serverless = Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME") != null || Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") != null
+		};
+
 		/// <summary>
 		/// Gets information about the current runtime environment including OS, platform, and configuration details.
 		/// </summary>
 		/// <returns>An AgentInfo object containing the environment information.</returns>
 		public static AgentInfo GetInfo()
 		{
-			return new AgentInfo
-			{
-				Hostname = Environment.MachineName,
-				Os = new Os
-				{
-					Version = Environment.OSVersion.VersionString,
-					Name = Environment.OSVersion.Platform.ToString()
-				},
-				Platform = new Platform
-				{
-					// version
-					Version = Environment.Version.ToString(),
-					// core or framework
-					Arch = Environment.Version.Major >= 5 ? "core" : "framework"
-				},
-				IpAddress = IPHelper.Server,
-				DryMode = EnvironmentHelper.DryMode,
-				// check if lambda or azure function
-				Serverless = Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME") != null || Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID") != null,
-				Library = "firewall-dotnet",
-				Version = "1.0.0"
-			};
+			// Update only the fields that can change
+			_cachedAgentInfo.DryMode = EnvironmentHelper.DryMode;
+
+			return _cachedAgentInfo;
 		}
-    }
+	}
 }
