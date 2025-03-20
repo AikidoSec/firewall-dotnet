f30326a7dbce02dc064e1e3af857d6836fe0c3f0
Yannis Standaert <yannis@quickminds.be>
2025-03-20 11:51:24 +0100

    more tests added

--- Aikido.Zen.Test/IPRangeTests.cs
+++ Aikido.Zen.Test/IPRangeTests.cs
@@ -181,5 +181,136 @@ namespace Aikido.Zen.Test
             Assert.That(IPHelper.IsPrivateOrLocalIp("0.0.0.0"), Is.True, "0.0.0.0 should be identified as private");
             Assert.That(IPHelper.IsPrivateOrLocalIp("0.1.2.3"), Is.True, "Address in 0.0.0.0/8 should be identified as private");
         }
+
+        [Test]
+        public void ShouldHandleDifferentCIDRNotations()
+        {
+            // Arrange
+            var ranges = new List<string> { "1.2.3.4/16", "1.2.3.4/15", "1.2.3.4/17" };
+
+            // Act
+            foreach (var range in ranges)
+            {
+                _ipRange.InsertRange(range);
+            }
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_ipRange.IsIpInRange("1.2.3.4"), Is.True, "Should match exact IP");
+                Assert.That(_ipRange.IsIpInRange("1.2.0.4"), Is.True, "Should match IP in /16 range");
+                Assert.That(_ipRange.IsIpInRange("1.2.3.0"), Is.True, "Should match IP in /16 range");
+            });
+        }
+
+        [Test]
+        public void ShouldHandleIPv6CIDRNotations()
+        {
+            // Arrange
+            var ranges = new List<string> { "1:2::/32", "1:2::/31", "1:2::/33" };
+
+            // Act
+            foreach (var range in ranges)
+            {
+                _ipRange.InsertRange(range);
+            }
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_ipRange.IsIpInRange("1:2::"), Is.True, "Should match exact IPv6");
+                Assert.That(_ipRange.IsIpInRange("1:2:0:0:0:0:0:0"), Is.True, "Should match IPv6 in /32 range");
+                Assert.That(_ipRange.IsIpInRange("1:2:ffff:ffff:ffff:ffff:ffff:ffff"), Is.True, "Should match IPv6 in /32 range");
+            });
+        }
+
+        [Test]
+        public void ShouldHandleZeroCIDRNotation()
+        {
+            // Arrange
+            var ranges = new List<string> { "0.0.0.0/0", "::/0" };
+
+            // Act
+            foreach (var range in ranges)
+            {
+                _ipRange.InsertRange(range);
+            }
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_ipRange.IsIpInRange("1.2.3.4"), Is.True, "Should match any IPv4 with /0");
+                Assert.That(_ipRange.IsIpInRange("255.255.255.255"), Is.True, "Should match any IPv4 with /0");
+                Assert.That(_ipRange.IsIpInRange("1:2:3:4:5:6:7:8"), Is.True, "Should match any IPv6 with /0");
+                Assert.That(_ipRange.IsIpInRange("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"), Is.True, "Should match any IPv6 with /0");
+            });
+        }
+
+        [Test]
+        public void ShouldHandleMixedIPv4AndIPv6()
+        {
+            // Arrange
+            var ranges = new List<string> { "1.2.3.4/16", "1:2:3:4:5:6:1.2.3.4/128" };
+
+            // Act
+            foreach (var range in ranges)
+            {
+                _ipRange.InsertRange(range);
+            }
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_ipRange.IsIpInRange("1.2.3.4"), Is.True, "Should match IPv4");
+                Assert.That(_ipRange.IsIpInRange("1.2.0.4"), Is.True, "Should match IPv4 in range");
+                Assert.That(_ipRange.IsIpInRange("1:2:3:4:5:6:1.2.3.4"), Is.True, "Should match exact IPv6");
+                Assert.That(_ipRange.IsIpInRange("1:2:3:4:5:6:1.2.3.5"), Is.False, "Should not match different IPv6");
+            });
+        }
+
+        [Test]
+        public void ShouldHandleOverlappingRangesWithDifferentCIDR()
+        {
+            // Arrange
+            var ranges = new List<string> { "1.2.3.4/16", "1.2.3.4/15", "1.2.3.4/17" };
+
+            // Act
+            foreach (var range in ranges)
+            {
+                _ipRange.InsertRange(range);
+            }
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_ipRange.IsIpInRange("1.2.3.4"), Is.True, "Should match exact IP");
+                Assert.That(_ipRange.IsIpInRange("1.2.0.4"), Is.True, "Should match IP in /16 range");
+                Assert.That(_ipRange.IsIpInRange("1.2.3.0"), Is.True, "Should match IP in /16 range");
+                Assert.That(_ipRange.IsIpInRange("1.2.4.0"), Is.True, "Should match IP in /15 range");
+                Assert.That(_ipRange.IsIpInRange("1.2.3.128"), Is.True, "Should match IP in /17 range");
+            });
+        }
+
+        [Test]
+        public void ShouldHandleZeroAddresses()
+        {
+            // Arrange
+            var ranges = new List<string> { "0.0.0.0/8", "::/128" };
+
+            // Act
+            foreach (var range in ranges)
+            {
+                _ipRange.InsertRange(range);
+            }
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_ipRange.IsIpInRange("0.0.0.0"), Is.True, "Should match IPv4 zero address");
+                Assert.That(_ipRange.IsIpInRange("0.1.2.3"), Is.True, "Should match IPv4 in zero range");
+                Assert.That(_ipRange.IsIpInRange("::"), Is.True, "Should match IPv6 zero address");
+                Assert.That(_ipRange.IsIpInRange("::1"), Is.False, "Should not match different IPv6");
+            });
+        }
     }
 }
