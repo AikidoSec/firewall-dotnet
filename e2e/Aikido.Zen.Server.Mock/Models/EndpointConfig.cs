5f9801b7b986db0a1aa0aba9cb8390895a81ad5e
Yannis Standaert <yannis@quickminds.be>
2025-05-06 13:19:52 +0200

    fix ratelimiting e2e tests

--- e2e/Aikido.Zen.Server.Mock/Models/EndpointConfig.cs
+++ e2e/Aikido.Zen.Server.Mock/Models/EndpointConfig.cs
@@ -5,7 +5,13 @@ public class EndpointConfig
     public string Method { get; set; }
     public string Route { get; set; }
     public bool ForceProtectionOff { get; set; }
-    public bool GraphQL { get; set; }
+    public GraphQLConfig? GraphQL { get; set; } = null;
     public IEnumerable<string> AllowedIPAddresses { get; set; }
     public RateLimitingConfig RateLimiting { get; set; }
+}
+
+public class GraphQLConfig
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
 }
