985aff1c082044fe53bd298d3a6c4f28d2f79f80
Yannis Standaert <yannis@quickminds.be>
2025-06-05 13:03:04 +0200

    added web forms sample app

---
+++ Webforms.Sample.App/Global.asax.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using Aikido.Zen.DotNetFramework;
+
+namespace Webforms.Sample.App
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Zen.Start();
+        }
+    }
+}
