8b5e0aec093ba3184bc1767ba72260445dc65d31
Yannis Standaert <yannis@quickminds.be>
2025-06-11 12:33:10 +0200

    more robust debug logging for .Net Framework

--- Aikido.Zen.Core/DefaultLogger.cs
+++ Aikido.Zen.Core/DefaultLogger.cs
@@ -1,25 +1,24 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 
 namespace Aikido.Zen.Core
 {
     internal class DefaultLogger : ILogger
     {
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return true;
-        }
-
+        public IDisposable BeginScope<TState>(TState state) => null;
+        public bool IsEnabled(LogLevel logLevel) => true;
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
-            Console.WriteLine(state);
+            var message = state.ToString();
+            if (formatter != null)
+            {
+                message = formatter(state, exception);
+            }
+            // log to console, by default it works for .Net core web apps
+            Console.WriteLine(message);
+            // log to debug, by default it works for .Net framework web apps
+            Debug.WriteLine(message);
         }
     }
 }
