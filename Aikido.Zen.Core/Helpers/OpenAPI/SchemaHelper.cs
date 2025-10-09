c5551335b04d2a97ea73dfdbfb057624118daab6
Strahinja Mijajlovic <strahinja@aikido.dev>
2025-10-09 17:54:26 +0200

    Remove unused references

--- Aikido.Zen.Core/Helpers/OpenAPI/SchemaHelper.cs
+++ Aikido.Zen.Core/Helpers/OpenAPI/SchemaHelper.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
-using System.Xml;
+
 using Aikido.Zen.Core.Models;
 
 namespace Aikido.Zen.Core.Helpers.OpenAPI
