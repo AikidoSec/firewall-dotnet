b3cd1e839ec22aa9ad713b686e09e291cdd32a9e
Strahinja Mijajlovic <strahinja@aikido.dev>
2025-10-31 17:38:32 +0100

    Rename the Praser into Handler Move the reporting stats into the Handler

--- Aikido.Zen.Core/Patches/LLMs/LLMResultParsers/BaseResponseHandler.cs
+++ Aikido.Zen.Core/Patches/LLMs/LLMResultParsers/BaseResponseHandler.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Reflection;
-
 using Aikido.Zen.Core.Helpers;
 using Aikido.Zen.Core.Models.LLMs;
 using Aikido.Zen.Core.Patches.LLMs.LLMResultParsers.Abstractions;
+using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Aikido.Zen.Core.Patches.LLMs.LLMResultParsers
 {
@@ -11,10 +11,20 @@ namespace Aikido.Zen.Core.Patches.LLMs.LLMResultParsers
     /// Base class for parsing LLM responses into <see cref="ParsedLLMResponseModel"/> instances.
     /// Provides basic implementations for extracting model name and token usage
     /// </summary>
-    internal abstract class BaseResponseParser : ILLMResponseParser
+    internal abstract class BaseResponseHandler : ILLMResponseHandler
     {
+        protected readonly string operationKind = "ai_op";
         protected const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase;
-        public abstract bool CanParse(string assembly);
+        public abstract bool CanHandle(string assembly);
+
+        /// <summary>
+        /// Determines whether the specified method name contains the word "stream",  using a case-insensitive
+        /// comparison.
+        /// </summary>
+        /// <param name="method">The method name to evaluate. Can be null.</param>
+        /// <returns><see langword="true"/> if the specified method name contains the word "stream";  otherwise, <see
+        /// langword="false"/>.</returns>
+        public virtual bool IsStreamMethod(string method) { return method?.IndexOf("stream", StringComparison.OrdinalIgnoreCase) >= 0; }
 
         /// <summary>
         /// Parses the incoming LLM result into a <see cref="ParsedLLMResponseModel"/> instance.
@@ -23,19 +33,29 @@ namespace Aikido.Zen.Core.Patches.LLMs.LLMResultParsers
         /// <param name="assembly">The assembly from which the call originated</param>
         /// <param name="method">Calling method from which the call originated</param>
         /// <returns>Parsed <see cref="ParsedLLMResponseModel"/>, or an empty model.</returns>
-        public virtual ParsedLLMResponseModel Parse(object result, string assembly, string method)
+        public virtual void Handle(object result, string assembly, MethodBase method, Context context)
         {
-            if (result == null)
+            var stopwatch = Stopwatch.StartNew();
+
+            if (context is null)
+                LogHelper.ErrorLog(Agent.Logger, $"OnLLMHandle: Context is null. Provider: {assembly} Method: {method}");
+
+            if (result == null)            
+                LogHelper.ErrorLog(Agent.Logger, $"OnLLMHandle: Result is null. Provider: {assembly} Method: {method}");    
+
+            if(IsStreamMethod(method.Name))
             {
-                LogHelper.ErrorLog(Agent.Logger, $"LLM result is null for the provider: {assembly}");
-                return new ParsedLLMResponseModel();
+                ParseStream(result, assembly, method, context, stopwatch);
+                return;
             }
 
-            var parsedResponse = new ParsedLLMResponseModel();
-            parsedResponse.Model = ParseModelName(result, assembly, method);
-            parsedResponse.TokenUsage = ParseTokenUsage(result, assembly, method);
+            var parsedResponse = new ParsedLLMResponseModel
+            {
+                Model = ParseModelName(result, assembly, method.Name),
+                TokenUsage = ParseTokenUsage(result, assembly, method.Name)
+            };
 
-            return parsedResponse;
+            ReportStats(parsedResponse, assembly, context, method, stopwatch);
         }
 
         /// <summary>
@@ -100,6 +120,33 @@ namespace Aikido.Zen.Core.Patches.LLMs.LLMResultParsers
                 LogHelper.ErrorLog(Agent.Logger, $"LLM Token Usage Parsing failed from the assembly: {assembly} Reason: {e.Message}");
             }
             return tokenUsage;
+        }
+
+        /// <summary>
+        /// Prases the LLM result object from a streaming method.
+        /// </summary>
+        /// <param name="result">The result of the LLM request</param>
+        /// <param name="assembly">The assembly from which the call originated</param>
+        /// <param name="method">Calling method from which the call originated</param>
+        protected virtual void ParseStream(object result, string assembly, MethodBase method, Context context, Stopwatch stopwatch)
+        {
+            LogHelper.ErrorLog(Agent.Logger, $"LLM Stream Parsing attempted using Base Handler for the Assembly: {assembly}, Method: {method}");
+        }
+
+        protected void ReportStats(ParsedLLMResponseModel parsedResponse, string assembly, Context context, MethodBase method, Stopwatch stopwatch)
+        {
+            // Record AI statistics
+            Agent.Instance.Context.OnAiCall(assembly, parsedResponse.Model, parsedResponse.TokenUsage.InputTokens, parsedResponse.TokenUsage.OutputTokens, context?.Route);
+
+            // record sink statistics
+            Agent.Instance.Context.OnInspectedCall(
+                operation: $"{method.DeclaringType.Namespace}.{method.DeclaringType.Name}.{method.Name}",
+                kind: operationKind,
+                durationInMs: stopwatch.ElapsedMilliseconds,
+                attackDetected: false,
+                blocked: false,
+                withoutContext: context == null
+            );
         }
     }
 }
