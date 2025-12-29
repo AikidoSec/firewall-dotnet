abe9c6cf2f660708cf357ddf9f808950ae53ba70
teta2k <teta2k@yahoo.com>
2025-12-29 17:19:06 +0100

    Explicit full url ToString

--- Aikido.Zen.DotNetFramework/HttpModules/ContextModule.cs
+++ Aikido.Zen.DotNetFramework/HttpModules/ContextModule.cs
@@ -73,7 +73,7 @@ namespace Aikido.Zen.DotNetFramework.HttpModules
                 var context = new Context
                 {
                     Url = httpContext.Request.Path,
-                    FullUrl = httpContext.Request.Url,
+                    FullUrl = httpContext.Request.Url.ToString(),
                     Method = httpContext.Request.HttpMethod,
                     Query = FlattenQueryParameters(httpContext.Request.QueryString),
                     Headers = FlattenHeaders(httpContext.Request.Headers),
