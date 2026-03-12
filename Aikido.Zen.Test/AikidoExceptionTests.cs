81287b636b4b03e0abb5674589b56642f35c1728
teta2k <teta2k@yahoo.com>
2026-03-12 16:04:42 +0100

    Fix more tests

--- Aikido.Zen.Test/AikidoExceptionTests.cs
+++ Aikido.Zen.Test/AikidoExceptionTests.cs
@@ -117,11 +117,11 @@ namespace Aikido.Zen.Test
 
             Assert.That(
                 exception.Message,
-                Is.EqualTo("Zen has blocked a server-side request forgery: HttpClient.SendAsync(...) originating from query.url"));
+                Is.EqualTo("Zen has blocked a server-side request forgery: HttpClient.SendAsync originating from query.url"));
         }
 
         [Test]
-        public void SSRFDetected_WithoutSource_ShouldReturnCorrectMessage()
+        public void SSRFDetected_WithUnknownSource_ShouldReturnCorrectMessage()
         {
             var exception = AikidoException.SSRFDetected(
                 "a stored server-side request forgery",
@@ -130,7 +130,7 @@ namespace Aikido.Zen.Test
 
             Assert.That(
                 exception.Message,
-                Is.EqualTo("Zen has blocked a stored server-side request forgery: HttpClient.SendAsync(...) originating from unknown source"));
+                Is.EqualTo("Zen has blocked a stored server-side request forgery: HttpClient.SendAsync originating from unknown source"));
         }
 
         [Test]
