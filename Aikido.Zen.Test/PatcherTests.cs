3bfab47a75746a0855e151d852a222c01f651c59
teta2k <teta2k@yahoo.com>
2026-05-10 03:14:29 +0200

    Add patch runtime integration coverage

--- Aikido.Zen.Test/PatcherTests.cs
+++ Aikido.Zen.Test/PatcherTests.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Threading;
 using Aikido.Zen.Core;
 using Aikido.Zen.Core.Api;
+using Aikido.Zen.Core.Exceptions;
 using Aikido.Zen.Core.Models;
 using Aikido.Zen.Core.Sinks;
 using Aikido.Zen.Tests.Mocks;
@@ -42,6 +43,9 @@ namespace Aikido.Zen.Test
             Environment.SetEnvironmentVariable("AIKIDO_REALTIME_URL", "http://localhost:3000");
 
             var reportingApiMock = new Mock<IReportingAPIClient>();
+            reportingApiMock
+                .Setup(r => r.ReportAsync(It.IsAny<string>(), It.IsAny<object>()))
+                .ReturnsAsync(new ReportingAPIResponse { Success = true });
             var runtimeApiMock = new Mock<IRuntimeAPIClient>();
             _agent = Agent.NewInstance(ZenApiMock.CreateMock(reportingApiMock.Object, runtimeApiMock.Object).Object);
             _agent.ClearContext();
@@ -161,6 +165,146 @@ namespace Aikido.Zen.Test
             AssertPostfixPatch(GetMethod(typeof(ScannerTarget), nameof(ScannerTarget.PostfixTarget)));
         }
 
+        [Test]
+        public void PatchedFileApis_BlockPathTraversalBeforeOriginalOperation()
+        {
+            Patcher.PatchSinks(() => _context);
+            var tempDirectory = Path.Combine(Path.GetTempPath(), $"aikido-patcher-{Guid.NewGuid():N}");
+
+            try
+            {
+                Directory.CreateDirectory(tempDirectory);
+                var sourcePath = Path.Combine(tempDirectory, "source.txt");
+                var destinationPath = Path.Combine(tempDirectory, "destination.txt");
+
+                File.WriteAllText(sourcePath, "safe");
+                Assert.That(File.ReadAllText(sourcePath), Is.EqualTo("safe"));
+
+                File.Copy(sourcePath, destinationPath, true);
+                Assert.That(File.ReadAllText(destinationPath), Is.EqualTo("safe"));
+
+                Assert.That(
+                    Path.GetFullPath("child.txt", tempDirectory),
+                    Is.EqualTo(Path.Combine(tempDirectory, "child.txt")));
+            }
+            finally
+            {
+                if (Directory.Exists(tempDirectory))
+                {
+                    Directory.Delete(tempDirectory, true);
+                }
+            }
+
+            _context.ParsedUserInput = new Dictionary<string, string>
+            {
+                { "query.path", "../secret.txt" }
+            };
+
+            var readException = Assert.Throws<AikidoException>(() =>
+                File.ReadAllText("/var/www/data/../secret.txt"));
+            Assert.That(readException?.Message, Does.Contain("Path traversal detected"));
+
+            var copyException = Assert.Throws<AikidoException>(() =>
+                File.Copy("/var/www/data/source.txt", "/var/www/data/../secret.txt", true));
+            Assert.That(copyException?.Message, Does.Contain("Path traversal detected"));
+
+            var fullPathException = Assert.Throws<AikidoException>(() =>
+                Path.GetFullPath("safe.txt", "/var/www/data/../secret.txt"));
+            Assert.That(fullPathException?.Message, Does.Contain("Path traversal detected"));
+        }
+
+        [Test]
+        public void PatchedHttpApis_BlockOutboundRequestBeforeNetworkCall()
+        {
+            Patcher.PatchSinks(() => _context);
+
+            using (var safeHttpClient = new HttpClient(new TestHttpMessageHandler()))
+            using (var safeRequest = new HttpRequestMessage(HttpMethod.Get, "https://safe.example/path"))
+            {
+                var response = safeHttpClient.Send(safeRequest, CancellationToken.None);
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
+
+            using (var clientWithBaseAddress = new HttpClient(new TestHttpMessageHandler()) { BaseAddress = new Uri("https://safe.example") })
+            {
+                var exception = Assert.Throws<ArgumentNullException>(() =>
+                    clientWithBaseAddress.Send(null!, CancellationToken.None));
+                Assert.That(exception?.ParamName, Is.EqualTo("request"));
+            }
+
+            _agent.Context.Config.UpdateOutboundDomains(false, new[]
+            {
+                new OutboundDomainConfig { Hostname = "blocked.example", Mode = "block" }
+            });
+
+            using (var httpClient = new HttpClient { BaseAddress = new Uri("https://blocked.example") })
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "/path"))
+            {
+                var httpClientException = Assert.Throws<AikidoException>(() =>
+                    httpClient.Send(request, CancellationToken.None));
+                Assert.That(httpClientException?.Message, Does.Contain("blocked.example"));
+            }
+
+#pragma warning disable SYSLIB0014
+            var webRequest = WebRequest.Create("https://blocked.example/path");
+#pragma warning restore SYSLIB0014
+            webRequest.Timeout = 1;
+
+            var webRequestException = Assert.Throws<AikidoException>(() => webRequest.GetResponse());
+            Assert.That(webRequestException?.Message, Does.Contain("blocked.example"));
+        }
+
+        [Test]
+        public void PatchedProcessStart_BlocksShellInjectionBeforeStartingProcess()
+        {
+            Patcher.PatchSinks(() => _context);
+
+            using (var safeProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo("definitely-not-a-real-aikido-command", "--safe")
+            })
+            {
+                Assert.Throws<System.ComponentModel.Win32Exception>(() => safeProcess.Start());
+            }
+
+            _context.ParsedUserInput = new Dictionary<string, string>
+            {
+                { "body.command", "$(echo)" }
+            };
+
+            using var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("definitely-not-a-real-aikido-command", "$(echo)")
+            };
+
+            var exception = Assert.Throws<AikidoException>(() => process.Start());
+            Assert.That(exception?.Message, Does.Contain("Shell injection detected"));
+        }
+
+        [Test]
+        public void PatchedDbCommandAsync_BlocksSqlInjectionBeforeExecutingCommand()
+        {
+            Patcher.PatchSinks(() => _context);
+
+            using (var safeCommand = new TestDbCommand { CommandText = "SELECT 1" })
+            {
+                Assert.DoesNotThrowAsync(async () => await safeCommand.ExecuteScalarAsync());
+            }
+
+            _context.ParsedUserInput = new Dictionary<string, string>
+            {
+                { "body.query", "1' OR '1'='1'" }
+            };
+
+            using var command = new TestDbCommand
+            {
+                CommandText = "SELECT * FROM users WHERE id = '1' OR '1'='1'"
+            };
+
+            var exception = Assert.ThrowsAsync<AikidoException>(async () => await command.ExecuteScalarAsync());
+            Assert.That(exception?.Message, Does.Contain("SQL injection detected"));
+        }
+
         private static MethodInfo GetMethod(Type type, string methodName, params Type[] parameterTypes)
         {
             var method = type.GetMethod(
@@ -201,6 +345,71 @@ namespace Aikido.Zen.Test
             public static string PostfixTarget()
             {
                 return "original";
+            }
+        }
+
+        private sealed class TestDbCommand : DbCommand
+        {
+            private readonly DbParameterCollection _parameters = new Mock<DbParameterCollection>().Object;
+            private string _commandText = string.Empty;
+
+            [System.Diagnostics.CodeAnalysis.AllowNull]
+            public override string CommandText
+            {
+                get => _commandText;
+                set => _commandText = value ?? string.Empty;
+            }
+
+            public override int CommandTimeout { get; set; }
+            public override System.Data.CommandType CommandType { get; set; }
+            public override bool DesignTimeVisible { get; set; }
+            public override System.Data.UpdateRowSource UpdatedRowSource { get; set; }
+            protected override DbConnection? DbConnection { get; set; }
+            protected override DbParameterCollection DbParameterCollection => _parameters;
+            protected override DbTransaction? DbTransaction { get; set; }
+
+            public override void Cancel()
+            {
+            }
+
+            public override int ExecuteNonQuery()
+            {
+                return 0;
+            }
+
+            public override object? ExecuteScalar()
+            {
+                return null;
+            }
+
+            public override void Prepare()
+            {
+            }
+
+            protected override DbParameter CreateDbParameter()
+            {
+                throw new NotSupportedException();
+            }
+
+            protected override DbDataReader ExecuteDbDataReader(System.Data.CommandBehavior behavior)
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        private sealed class TestHttpMessageHandler : HttpMessageHandler
+        {
+            protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    RequestMessage = request
+                };
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(Send(request, cancellationToken));
             }
         }
 
