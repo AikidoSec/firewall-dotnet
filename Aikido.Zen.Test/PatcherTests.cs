8007ea90bd7c1e36109a197f6053f8a7b97470ce
teta2k <teta2k@yahoo.com>
2026-05-10 18:55:40 +0200

    Expand patch catalog coverage

--- Aikido.Zen.Test/PatcherTests.cs
+++ Aikido.Zen.Test/PatcherTests.cs
@@ -3,7 +3,10 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
 using System.Threading;
 using Aikido.Zen.Core;
 using Aikido.Zen.Core.Api;
@@ -20,6 +23,9 @@ namespace Aikido.Zen.Test
     public class PatcherTests
     {
         private const string HarmonyId = "aikido.zen";
+        private static readonly Lazy<Type> OpenAIChatClientTarget = new Lazy<Type>(CreateOpenAIChatClientTarget);
+        private static readonly Lazy<Type> NPocoDatabaseTarget = new Lazy<Type>(CreateNPocoDatabaseTarget);
+        private static readonly Lazy<(Type DatabaseFacade, Type Extensions)> EFCoreRawSqlTarget = new Lazy<(Type DatabaseFacade, Type Extensions)>(CreateEFCoreRawSqlTarget);
         private Context _context;
         private Agent _agent;
 
@@ -232,6 +238,15 @@ namespace Aikido.Zen.Test
                 Assert.That(exception?.ParamName, Is.EqualTo("request"));
             }
 
+            using (var safeWebServer = new TestHttpServer())
+            {
+#pragma warning disable SYSLIB0014
+                var safeWebRequest = WebRequest.Create(safeWebServer.Url);
+#pragma warning restore SYSLIB0014
+                using var safeWebResponse = (HttpWebResponse)safeWebRequest.GetResponse();
+                Assert.That(safeWebResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
+
             _agent.Context.Config.UpdateOutboundDomains(false, new[]
             {
                 new OutboundDomainConfig { Hostname = "blocked.example", Mode = "block" }
@@ -303,6 +318,108 @@ namespace Aikido.Zen.Test
 
             var exception = Assert.ThrowsAsync<AikidoException>(async () => await command.ExecuteScalarAsync());
             Assert.That(exception?.Message, Does.Contain("SQL injection detected"));
+        }
+
+        [Test]
+        public void PatchSinks_PatchesOptionalLlmTargetWhenAssemblyIsLoaded()
+        {
+            var chatClientType = OpenAIChatClientTarget.Value;
+            var result = new TestLLMResult
+            {
+                Model = "gpt-4o-mini",
+                Usage = new TestLLMUsage { InputTokenCount = 11, OutputTokenCount = 13 }
+            };
+            chatClientType.GetField("Result")!.SetValue(null, result);
+
+            Patcher.PatchSinks(() => _context);
+
+            var client = Activator.CreateInstance(chatClientType);
+            var returnedResult = chatClientType.GetMethod("CompleteChat")!.Invoke(client, null);
+
+            Assert.That(returnedResult, Is.SameAs(result));
+
+            var aiInfo = _agent.Context.AiStats.Providers.Values.Single();
+            Assert.That(aiInfo.Provider, Is.EqualTo("openai"));
+            Assert.That(aiInfo.Model, Is.EqualTo("gpt-4o-mini"));
+            Assert.That(aiInfo.Tokens.Input, Is.EqualTo(11));
+            Assert.That(aiInfo.Tokens.Output, Is.EqualTo(13));
+        }
+
+        [Test]
+        public void PatchSinks_PatchesOptionalSqlTargetsWhenAssembliesAreLoaded()
+        {
+            var npocoDatabaseType = NPocoDatabaseTarget.Value;
+            var npocoHelper = (Func<DbCommand, object>)npocoDatabaseType
+                .GetMethod("ExecuteScalarHelper")!
+                .CreateDelegate(typeof(Func<DbCommand, object>), Activator.CreateInstance(npocoDatabaseType));
+            var npocoExecuted = npocoDatabaseType.GetField("Executed")!;
+
+            var efCoreTarget = EFCoreRawSqlTarget.Value;
+            var efCoreMethod = efCoreTarget.Extensions.GetMethod("ExecuteSqlRaw")!;
+            var efCoreExecuted = efCoreTarget.Extensions.GetField("Executed")!;
+            var databaseFacade = Activator.CreateInstance(efCoreTarget.DatabaseFacade);
+
+            Patcher.PatchSinks(() => _context);
+
+            npocoExecuted.SetValue(null, false);
+            npocoHelper(new TestDbCommand { CommandText = "SELECT 1" });
+            Assert.That(npocoExecuted.GetValue(null), Is.EqualTo(true));
+
+            efCoreExecuted.SetValue(null, false);
+            var efCoreResult = efCoreMethod.Invoke(null, new object[] { databaseFacade!, "SELECT 1", Enumerable.Empty<object>() });
+            Assert.That(efCoreResult, Is.EqualTo(1));
+            Assert.That(efCoreExecuted.GetValue(null), Is.EqualTo(true));
+
+            _context.ParsedUserInput = new Dictionary<string, string>
+            {
+                { "body.query", "1' OR '1'='1'" }
+            };
+
+            npocoExecuted.SetValue(null, false);
+            var npocoException = Assert.Throws<AikidoException>(() =>
+                npocoHelper(new TestDbCommand { CommandText = "SELECT * FROM users WHERE id = '1' OR '1'='1'" }));
+            Assert.That(npocoException?.Message, Does.Contain("SQL injection detected"));
+            Assert.That(npocoExecuted.GetValue(null), Is.EqualTo(false));
+
+            efCoreExecuted.SetValue(null, false);
+            var efCoreException = Assert.Throws<TargetInvocationException>(() =>
+                efCoreMethod.Invoke(null, new object[] { databaseFacade!, "SELECT * FROM users WHERE id = '1' OR '1'='1'", Enumerable.Empty<object>() }));
+            Assert.That(efCoreException?.InnerException, Is.TypeOf<AikidoException>());
+            Assert.That(efCoreException?.InnerException?.Message, Does.Contain("SQL injection detected"));
+            Assert.That(efCoreExecuted.GetValue(null), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void PatchCatalog_ContinuesWhenPatchApplicationFails()
+        {
+            Assert.DoesNotThrow(() => Patcher.PatchCatalog(typeof(BrokenPatchCatalog)));
+
+            Assert.That(BrokenPatchTarget.ValidTarget(), Is.EqualTo("patched-after-error"));
+        }
+
+        [Test]
+        public void PatchCatalog_SkipsUnsafeAssemblyNamesAndStillPatchesValidTargets()
+        {
+            Assert.DoesNotThrow(() => Patcher.PatchCatalog(typeof(UnsafeAssemblyCatalog)));
+
+            Assert.That(UnsafeAssemblyTarget.ValidTarget(), Is.EqualTo("patched-after-unsafe-assembly"));
+        }
+
+        [Test]
+        public void PatchCatalog_WhenParameterTypesAreNotDeclared_PatchesLargestOverload()
+        {
+            Patcher.PatchCatalog(typeof(OverloadFallbackCatalog));
+
+            Assert.That(OverloadFallbackTarget.Execute("value"), Is.EqualTo("one-argument"));
+            Assert.That(OverloadFallbackTarget.Execute("value", 1), Is.EqualTo("patched-overload"));
+        }
+
+        [Test]
+        public void PatchCatalog_IgnoresInvalidCatalogEntriesAndStillPatchesValidTargets()
+        {
+            Assert.DoesNotThrow(() => Patcher.PatchCatalog(typeof(InvalidCatalogEntriesCatalog)));
+
+            Assert.That(InvalidCatalogTarget.ValidTarget(), Is.EqualTo("patched-valid-entry"));
         }
 
         private static MethodInfo GetMethod(Type type, string methodName, params Type[] parameterTypes)
@@ -410,6 +527,242 @@ namespace Aikido.Zen.Test
             protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
             {
                 return Task.FromResult(Send(request, cancellationToken));
+            }
+        }
+
+        private sealed class TestHttpServer : IDisposable
+        {
+            private readonly TcpListener _listener;
+            private readonly Task _responseTask;
+
+            public TestHttpServer()
+            {
+                _listener = new TcpListener(IPAddress.Loopback, 0);
+                _listener.Start();
+                Url = $"http://127.0.0.1:{((IPEndPoint)_listener.LocalEndpoint).Port}/";
+                _responseTask = Task.Run(HandleRequest);
+            }
+
+            public string Url { get; }
+
+            public void Dispose()
+            {
+                _listener.Stop();
+                _responseTask.Wait(TimeSpan.FromSeconds(1));
+            }
+
+            private async Task HandleRequest()
+            {
+                try
+                {
+                    using var client = await _listener.AcceptTcpClientAsync();
+                    using var stream = client.GetStream();
+
+                    var response = Encoding.ASCII.GetBytes("HTTP/1.1 200 OK\r\nContent-Length: 2\r\nConnection: close\r\n\r\nOK");
+                    await stream.WriteAsync(response.AsMemory(0, response.Length));
+                }
+                catch (ObjectDisposedException)
+                {
+                }
+                catch (SocketException)
+                {
+                }
+            }
+        }
+
+        private sealed class TestLLMResult
+        {
+            public string Model { get; set; } = string.Empty;
+            public TestLLMUsage Usage { get; set; } = new TestLLMUsage();
+        }
+
+        private sealed class TestLLMUsage
+        {
+            public int InputTokenCount { get; set; }
+            public int OutputTokenCount { get; set; }
+        }
+
+        private static Type CreateOpenAIChatClientTarget()
+        {
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("OpenAI"), AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("OpenAI");
+            var typeBuilder = moduleBuilder.DefineType("OpenAI.Chat.ChatClient", TypeAttributes.Public | TypeAttributes.Class);
+            var resultField = typeBuilder.DefineField("Result", typeof(object), FieldAttributes.Public | FieldAttributes.Static);
+
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+            var methodBuilder = typeBuilder.DefineMethod("CompleteChat", MethodAttributes.Public, typeof(object), Type.EmptyTypes);
+            var il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldsfld, resultField);
+            il.Emit(OpCodes.Ret);
+
+            return typeBuilder.CreateTypeInfo()!.AsType();
+        }
+
+        private static Type CreateNPocoDatabaseTarget()
+        {
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("NPoco"), AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("NPoco");
+            var typeBuilder = moduleBuilder.DefineType("NPoco.Database", TypeAttributes.Public | TypeAttributes.Class);
+            var executedField = typeBuilder.DefineField("Executed", typeof(bool), FieldAttributes.Public | FieldAttributes.Static);
+
+            typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+            var methodBuilder = typeBuilder.DefineMethod("ExecuteScalarHelper", MethodAttributes.Public, typeof(object), new[] { typeof(DbCommand) });
+            methodBuilder.DefineParameter(1, ParameterAttributes.None, "cmd");
+            var il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Stsfld, executedField);
+            il.Emit(OpCodes.Ldnull);
+            il.Emit(OpCodes.Ret);
+
+            return typeBuilder.CreateTypeInfo()!.AsType();
+        }
+
+        private static (Type DatabaseFacade, Type Extensions) CreateEFCoreRawSqlTarget()
+        {
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Microsoft.EntityFrameworkCore.Relational"), AssemblyBuilderAccess.Run);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("Microsoft.EntityFrameworkCore.Relational");
+
+            var databaseFacadeBuilder = moduleBuilder.DefineType("Microsoft.EntityFrameworkCore.Infrastructure.DatabaseFacade", TypeAttributes.Public | TypeAttributes.Class);
+            databaseFacadeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+            var databaseFacadeType = databaseFacadeBuilder.CreateTypeInfo()!.AsType();
+
+            var extensionsBuilder = moduleBuilder.DefineType(
+                "Microsoft.EntityFrameworkCore.RelationalDatabaseFacadeExtensions",
+                TypeAttributes.Public | TypeAttributes.Abstract | TypeAttributes.Sealed);
+            var executedField = extensionsBuilder.DefineField("Executed", typeof(bool), FieldAttributes.Public | FieldAttributes.Static);
+
+            var methodBuilder = extensionsBuilder.DefineMethod(
+                "ExecuteSqlRaw",
+                MethodAttributes.Public | MethodAttributes.Static,
+                typeof(int),
+                new[] { databaseFacadeType, typeof(string), typeof(IEnumerable<object>) });
+            methodBuilder.DefineParameter(1, ParameterAttributes.None, "databaseFacade");
+            methodBuilder.DefineParameter(2, ParameterAttributes.None, "sql");
+            methodBuilder.DefineParameter(3, ParameterAttributes.None, "parameters");
+            var il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Stsfld, executedField);
+            il.Emit(OpCodes.Ldc_I4_1);
+            il.Emit(OpCodes.Ret);
+
+            return (databaseFacadeType, extensionsBuilder.CreateTypeInfo()!.AsType());
+        }
+
+        private static class BrokenPatchTarget
+        {
+            public static string ValidTarget()
+            {
+                return "original";
+            }
+        }
+
+        private static class BrokenPatchCatalog
+        {
+            [SinkPrefix("", "Aikido.Zen.Test.PatcherTests+BrokenPatchTarget", nameof(BrokenPatchTarget.ValidTarget))]
+            private static bool BrokenPrefix(string missingArgument)
+            {
+                return true;
+            }
+
+            [SinkPrefix("", "Aikido.Zen.Test.PatcherTests+BrokenPatchTarget", nameof(BrokenPatchTarget.ValidTarget))]
+            private static bool ValidPrefix(ref string __result)
+            {
+                __result = "patched-after-error";
+                return false;
+            }
+        }
+
+        private static class UnsafeAssemblyTarget
+        {
+            public static string ValidTarget()
+            {
+                return "original";
+            }
+        }
+
+        private static class UnsafeAssemblyCatalog
+        {
+            [SinkPrefix("../unsafe", "Aikido.Zen.Test.PatcherTests+UnsafeAssemblyTarget", nameof(UnsafeAssemblyTarget.ValidTarget))]
+            private static bool UnsafeAssemblyPrefix(ref string __result)
+            {
+                __result = "should-not-patch";
+                return false;
+            }
+
+            [SinkPrefix("", "Aikido.Zen.Test.PatcherTests+UnsafeAssemblyTarget", nameof(UnsafeAssemblyTarget.ValidTarget))]
+            private static bool ValidPrefix(ref string __result)
+            {
+                __result = "patched-after-unsafe-assembly";
+                return false;
+            }
+        }
+
+        private static class OverloadFallbackTarget
+        {
+            public static string Execute(string value)
+            {
+                return "one-argument";
+            }
+
+            public static string Execute(string value, int count)
+            {
+                return "two-arguments";
+            }
+        }
+
+        private static class OverloadFallbackCatalog
+        {
+            [SinkPrefix("", "Aikido.Zen.Test.PatcherTests+OverloadFallbackTarget", nameof(OverloadFallbackTarget.Execute))]
+            private static bool Prefix(ref string __result)
+            {
+                __result = "patched-overload";
+                return false;
+            }
+        }
+
+        private static class InvalidCatalogTarget
+        {
+            public static string ValidTarget()
+            {
+                return "original";
+            }
+        }
+
+        private sealed class UnsupportedSinkPatchAttribute : SinkTargetAttribute
+        {
+            public UnsupportedSinkPatchAttribute(
+                string assemblyName,
+                string targetTypeName,
+                string targetMethodName,
+                params string[] targetParameterTypeNames)
+                : base(HarmonyPatchType.Transpiler, assemblyName, targetTypeName, targetMethodName, targetParameterTypeNames)
+            {
+            }
+        }
+
+        private static class InvalidCatalogEntriesCatalog
+        {
+#pragma warning disable CS8625
+            [SinkPrefix("", null, nameof(InvalidCatalogTarget.ValidTarget))]
+#pragma warning restore CS8625
+            private static bool NullTargetPrefix(ref string __result)
+            {
+                __result = "should-not-patch";
+                return false;
+            }
+
+            [UnsupportedSinkPatch("", "Aikido.Zen.Test.PatcherTests+InvalidCatalogTarget", nameof(InvalidCatalogTarget.ValidTarget))]
+            private static bool UnsupportedPatchTypePrefix(ref string __result)
+            {
+                __result = "should-not-patch";
+                return false;
+            }
+
+            [SinkPrefix("", "Aikido.Zen.Test.PatcherTests+InvalidCatalogTarget", nameof(InvalidCatalogTarget.ValidTarget))]
+            private static bool ValidPrefix(ref string __result)
+            {
+                __result = "patched-valid-entry";
+                return false;
             }
         }
 
