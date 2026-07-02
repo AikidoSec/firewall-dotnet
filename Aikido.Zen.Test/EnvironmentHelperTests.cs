8de4b7dd8d124d86a6ff74ef1405662b6f4ba8ed
Timo Kössler <info@timokoessler.de>
2026-07-02 20:01:20 +0200

    Add support for data regions (#356)
    * Add support for data regions
    
    * Add E2E coverage for data region endpoints
    
    * Keep data region E2E host alive across global patches
    
    ---------
    
    Co-authored-by: teta2k <teta2k@yahoo.com>

--- Aikido.Zen.Test/EnvironmentHelperTests.cs
+++ Aikido.Zen.Test/EnvironmentHelperTests.cs
@@ -62,12 +62,60 @@ namespace Aikido.Zen.Test.Helpers
         {
             // Arrange
             Environment.SetEnvironmentVariable("AIKIDO_ENDPOINT", null);
+            Environment.SetEnvironmentVariable("AIKIDO_TOKEN", null);
 
             // Act
             var url = EnvironmentHelper.AikidoUrl;
 
             // Assert
             Assert.That(url, Is.EqualTo("https://guard.aikido.dev"));
+        }
+
+        [TestCase("AIK_RUNTIME_1_2_US_random", "https://guard.us.aikido.dev")]
+        [TestCase("AIK_RUNTIME_1_2_ME_random", "https://guard.me.aikido.dev")]
+        [TestCase("AIK_RUNTIME_1_2_AU_random", "https://guard.au.aikido.dev")]
+        [TestCase("AIK_RUNTIME_1_2_EU_random", "https://guard.aikido.dev")]
+        [TestCase("AIK_RUNTIME_1_2_random", "https://guard.aikido.dev")] // old format without region
+        [TestCase(null, "https://guard.aikido.dev")]
+        [TestCase("not_a_runtime_token", "https://guard.aikido.dev")]
+        public void AikidoUrl_ShouldBeDerivedFromTokenRegion_WhenEndpointIsNotSet(string? token, string expectedUrl)
+        {
+            // Arrange
+            var originalEndpoint = Environment.GetEnvironmentVariable("AIKIDO_ENDPOINT");
+            var originalToken = Environment.GetEnvironmentVariable("AIKIDO_TOKEN");
+
+            try
+            {
+                Environment.SetEnvironmentVariable("AIKIDO_ENDPOINT", null);
+                Environment.SetEnvironmentVariable("AIKIDO_TOKEN", token);
+
+                // Act
+                var url = EnvironmentHelper.AikidoUrl;
+
+                // Assert
+                Assert.That(url, Is.EqualTo(expectedUrl));
+            }
+            finally
+            {
+                Environment.SetEnvironmentVariable("AIKIDO_ENDPOINT", originalEndpoint);
+                Environment.SetEnvironmentVariable("AIKIDO_TOKEN", originalToken);
+            }
+        }
+
+        [TestCase("AIK_RUNTIME_1_2_US_random", "US")]
+        [TestCase("AIK_RUNTIME_1_2_ME_random", "ME")]
+        [TestCase("AIK_RUNTIME_1_2_AU_random", "AU")]
+        [TestCase("AIK_RUNTIME_1_2_random", "EU")] // old format without region
+        [TestCase(null, "EU")]
+        [TestCase("", "EU")]
+        [TestCase("not_a_runtime_token", "EU")]
+        public void ExtractRegionFromToken_ShouldReturnExpectedRegion(string? token, string expectedRegion)
+        {
+            // Act
+            var region = EnvironmentHelper.ExtractRegionFromToken(token);
+
+            // Assert
+            Assert.That(region, Is.EqualTo(expectedRegion));
         }
 
         [Test]
